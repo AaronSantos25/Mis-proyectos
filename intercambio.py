4586325fb461493c9aa0a7c7e7edff6f44498290
Aaron Santos <143747616+AaronSantos25@users.noreply.github.com>
2023-12-25 21:43:45 -0400

    intercambio.py

---
+++ intercambio.py
@@ -0,0 +1,14 @@
+valor1 = 30
+valor2 = 70
+
+print("Valores iniciales:")
+print("Valor 1:", valor1)
+print("Valor 2:", valor2)
+
+temp = valor1
+valor1 = valor2
+valor2 = temp
+
+print("\nValores después del traspaso:")
+print("Valor 1:", valor1)
+print("Valor 2:", valor2)
